ba93328b8344f7e87ffa44939a33f006d2fb48f7
jay <davejay1412@gmail.com>
2020-12-21 13:33:42 +0530

    port fixed

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/',(req,res)=>{
     res.render('index',{layout : false});
 })
 
-
-app.listen(3000,()=>{
+let port  = process.env.PORT || 3000
+app.listen(port,()=>{
     console.log("listening at 3000")
 })
 
